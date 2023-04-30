b35abadf11a717f0be8915bbf6b7de631a221f2b
syphyr <syphyr@gmail.com>
2023-04-30 23:29:32 +0200

    Only build arm64-v8a for release

--- tor-droid-make.sh
+++ tor-droid-make.sh
@@ -250,7 +250,8 @@ show_options()
 }
 
 option=$1
-default_abis="arm64-v8a armeabi-v7a x86 x86_64"
+#default_abis="arm64-v8a armeabi-v7a x86 x86_64"
+default_abis="arm64-v8a"
 abis=$default_abis
 build_type="debug"
 
